9d3160118d47ad0e1e516d40efe334dd170ab156
yusifizcode <5ba0gh2@code.edu.az>
2024-03-06 23:25:09 +0400

    DataTypes, Variables, Naming conventions, FlowChart

---
+++ VariablesAndOperators/VariablesAndOperators/Program.cs
@@ -0,0 +1,138 @@
+﻿namespace VariablesAndOperators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // VARIABLES
+
+            //int a = 5;
+            //a = 4;
+            //short b = 15;
+            //byte c = 100;
+
+            //int a,b,c,d;
+            //a = 12;
+
+            //char letter = '#';
+            //string sentence = "Hello, AB204";
+            //bool check = true;
+            //bool check1 = false;
+
+
+            //int a = 5;
+            //int b = 5;
+            //int c = a + b;
+            //string text = "lorem, ipsum dolor sit amet!";
+
+            //Console.WriteLine();
+
+
+            // OPERATORS
+            // - Aritmethic
+            //int a = 5;
+            //int b = 15;
+            //int sum = a + b;
+            //int diff = a - b;
+            //int product = a * b;
+            //int divide = b / a;
+
+            //int mod = a % b;
+
+            //a++;
+            // a = a + 1;
+
+            //a--;
+            // a = a - 1;
+
+            //int c = a-- + 5;
+            //int d = --a + 5;
+            //Console.WriteLine(a);
+            //Console.WriteLine(d);
+            //Console.WriteLine(c);
+            //Console.WriteLine(c);
+
+            // Assignment
+
+            //int w;
+
+            //var x = 10;
+
+            //char c = 'c';
+            //char f = 'f';
+
+            //c = f;
+            //Console.WriteLine(c);
+
+            //int a = 5;
+            //a += 15;
+            //// a = a + 15;
+            //Console.WriteLine(a); // 20
+
+            //a -= 3;
+            //// a = a - 3;
+            //Console.WriteLine(a); // 17
+
+            //a *= 4;
+            //Console.WriteLine(a); // 68
+
+            //a /= 5;
+            //Console.WriteLine(a); // 13, eger a double olsa idi, 13.6
+
+            //a %= 3;
+            //Console.WriteLine(a); // 1, eger a double olsa idi, 1.5999999999999996
+
+            //var b = a == 1; // beraberdir mi?
+            //Console.WriteLine(b); // True
+
+            //b = a == 2; // beraberdir mi?
+            //Console.WriteLine(b); // False
+
+            //b = a != 1; // beraber deyil mi?
+            //Console.WriteLine(b); // False
+
+            //b = a != 2; // beraber deyil mi?
+            //Console.WriteLine(b); // True
+
+            //Console.WriteLine("----------------");
+
+            //b = a > 0;
+            //Console.WriteLine(b); // True
+
+            //b = a < 3;
+            //Console.WriteLine(b); // True
+
+            //b = a >= 1;
+            //Console.WriteLine(b); // True
+
+            //b = a <= 3;
+            //Console.WriteLine(b); // True
+
+            //Console.WriteLine("----------------");
+
+            //b = a >= 5 || a < 10; // ve ya - "+"
+            //Console.WriteLine(b);
+
+            //b = a >= 5 && a < 10; // ve  "*"
+            //Console.WriteLine(b);
+
+            //b = !(a >= 5 && a < 10);  // neticeni tersine cevirir
+            //Console.WriteLine(b);
+
+            //b = !(a >= 5 && a < 10 && a < 4 || a > 0);  // False
+            //Console.WriteLine(b);
+
+            //int yeddiyeBolunenEdedler = 0; // camelCase
+            //string soz_ve_cumle = ""; // snake_case
+            //float TamEdedDeyilAxi = 0; // PascalCase
+            //char bu-bir-chardir  kabab case
+
+            //double a = 3.4d;
+            //float b = 3.4f;
+            //decimal c = 3.4m;
+            //int a = 5;
+            //long c = 5;
+            //uint d = 123u;
+        }
+    }
+}
