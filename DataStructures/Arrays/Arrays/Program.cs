87746e05b3dbfa6e174c789f1fd6c665b387944b
yusifizcode <5ba0gh2@code.edu.az>
2024-03-11 19:23:30 +0400

    Arrays

---
+++ DataStructures/Arrays/Arrays/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1ci usul
+            //int[] arr = { 1, 2, 3, 4, 5 };
+
+            //Console.WriteLine(arr[4]);
+
+
+            // 2ci usul
+            //int[] arr2 = new int[5];
+
+            //arr2[0] = 3;
+            //arr2[1] = 5;
+            //arr2[2] = 6;
+
+            //Console.WriteLine(arr2[2]);
+
+            // 3cu usul
+            //int[] arr3 = new int[3] { 0, 1, 2 };
+
+            // 4cu usul
+            //int[] arr4 = new int[] { 0,2,3,5,6,7};
+
+
+            //string[] arr = { "Eli Memmedov", "Mehriban Seferova", "Araz Hummetov", "Almaye Memmedli", "Zaman Seferov", "Lale Gulahmedova" };
+
+            //string fullName = "Eli Memmedov";
+            //char[] chars = { 'E', 'l', 'i', ' ', 'M' };
+
+            //Console.WriteLine(arr.Length);
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    for (int j = 0; j < arr[i].Length; j++)
+            //    {
+            //        Console.WriteLine(arr[i][j]);
+            //    }
+            //}
+
+
+            //int[] arr = new int[5] { 10, 11, 12, 13, 14 };
+            //int sum = 0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    sum = sum + arr[i];
+            //}
+            //int avg = sum / arr.Length;
+
+            //Console.WriteLine(avg);
+
+
+            //int[,] arr = {
+            //    { 1, 2 },
+            //    { 2, 3 },
+            //    { 3, 4 },
+            //    { 4, 5 },
+            //};
+
+            //Console.WriteLine(arr.GetLength(2));
+            //for (int i = 0; i < 4; i++)
+            //{
+            //    for (int j = 0; j < 2; j++)
+            //    {
+            //        Console.WriteLine(arr[i, j]);
+            //    }
+            //}
+
+            //int[] arr = { 11, 224, 33, 44, 55 };
+            //int sum = 0;
+            //int count = 0;
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[i]);
+            //}
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        sum += arr[i];
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine(sum / count);
+        }
+    }
+}
